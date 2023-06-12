bffff6de53ad5f9f791b2f73011abae7a7ea801a
yeqing <heopro@163.com>
2023-06-12 22:42:12 +0800

    arrow-keys-int

--- kilo.c
+++ kilo.c
@@ -16,10 +16,10 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
-    ARROW_LEFT = 'a',
-    ARROW_RIGHT = 'd',
-    ARROW_UP = 'w',
-    ARROW_DOWN = 's'
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
 };
 
 /*** data ***/
@@ -80,7 +80,7 @@ void enableRawMode() {
     };
 }
 
-char editorReadKey() {
+int editorReadKey() {
     int nread;
     char c;
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
@@ -221,7 +221,7 @@ void editorRefreshScreen() {
 
 /*** input ***/
 
-void editorMoveCursor(char key) {
+void editorMoveCursor(int key) {
     switch (key) {
         case ARROW_LEFT:
             E.cx--;
@@ -239,7 +239,7 @@ void editorMoveCursor(char key) {
 }
 
 void editorProcessKeypress() {
-    char c = editorReadKey();
+    int c = editorReadKey();
 
     switch (c) {
         case CTRL_KEY('q'):

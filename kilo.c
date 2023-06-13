7bc680abe9d40b57d3dcc0cd8f1a43f2ea181c0c
yeqing <heopro@163.com>
2023-06-13 22:45:54 +0800

    use-rx

--- kilo.c
+++ kilo.c
@@ -386,7 +386,7 @@ void editorRefreshScreen() {
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     // ?25h 展示光标

b15a8dc46e7356d791101d96d2e981ed3afeef8e
yeqing <heopro@163.com>
2023-06-13 20:41:16 +0800

    feature-test-macros

--- kilo.c
+++ kilo.c
@@ -1,5 +1,9 @@
 /*** includes ***/
 
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -212,6 +216,7 @@ void editorOpen(char *filename) {
     ssize_t linelen;
     linelen = getline(&line, &linecap, fp);
     if (linelen != -1) {
+        // 去掉后面的 \r 和 \n
         while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
             linelen--;
         }

76bb9b8b5a8cfcb19b80bbc93ed657bd51179315
yeqing <heopro@163.com>
2023-06-19 22:54:24 +0800

    decimal-point

--- kilo.c
+++ kilo.c
@@ -265,7 +265,7 @@ void editorUpdateSyntax(erow *row) {
         char c = row->render[i];
 
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
-        if (isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) {
+        if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
             row->hl[i] = HL_NUMBER;
             i++;
             prev_sep = 0;
@@ -279,9 +279,12 @@ void editorUpdateSyntax(erow *row) {
 
 int editorSyntaxToColor(int hl) {
     switch (hl) {
-        case HL_NUMBER: return 31;
-        case HL_MATCH: return 34;
-        default: return 37;
+        case HL_NUMBER:
+            return 31;
+        case HL_MATCH:
+            return 34;
+        default:
+            return 37;
     }
 }
 

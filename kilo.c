8b1658b73c78fbbe20064a3ce6c96408c97f5b99
yeqing <heopro@163.com>
2023-06-20 20:32:10 +0800

    c-keywords

--- kilo.c
+++ kilo.c
@@ -62,6 +62,7 @@ enum editorHighlight {
 struct editorSyntax {
     char *filetype;
     char **filematch;
+    char **keywords;
     char *singleline_comment_start;
     int flags;
 };
@@ -105,11 +106,18 @@ struct editorConfig E;
 /*** filetypes ***/
 
 char *C_HL_extensions[] = {".c", ".h", ".cpp", NULL};
+char *C_HL_keywords[] = {
+        "switch", "if", "while", "for", "break", "continue", "return", "else",
+        "struct", "union", "typedef", "static", "enum", "class", "case",
+        "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+        "void|", NULL
+};
 
 struct editorSyntax HLDB[] = {
         {
                 "c",
                 C_HL_extensions,
+                C_HL_keywords,
                 "//",
                 HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
         },
